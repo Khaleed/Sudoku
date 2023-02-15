bfda5072625d0f50910796ce05f97e7a94107ea4
Khalid <khalidali@consultant.com>
2023-02-15 17:59:11 +0100

    Implement recursive backtracker for getSolved method.

--- src/main/java/com/sudoku/gamelogic/SudokuConfigurator.java
+++ src/main/java/com/sudoku/gamelogic/SudokuConfigurator.java
@@ -4,15 +4,14 @@ import com.sudoku.model.Coordinates;
 import com.sudoku.model.Sudoku;
 import com.sudoku.utilities.Helper;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Random;
 
+import static com.sudoku.gamelogic.GameLogic.*;
+
 /** Class that manages the configuration of a Sudoku Game. */
 public class SudokuConfigurator {
-    private static final Random RANDOM = new Random(System.currentTimeMillis());
-
     public static int[][] getNewGameBoard() {
         return initGame(getSolvedGame());
     }
@@ -34,10 +33,11 @@ public class SudokuConfigurator {
     }
 
     private static void removeValsFromBoard(int[][] solvableBoard) {
+        Random random = new Random(System.currentTimeMillis());
         int i = 0;
         while (i < 40) {
-            int nextX = RANDOM.nextInt(Sudoku.LIMIT);
-            int nextY = RANDOM.nextInt(Sudoku.LIMIT);
+            int nextX = random.nextInt(Sudoku.LIMIT);
+            int nextY = random.nextInt(Sudoku.LIMIT);
             if (!isSquareEmpty(solvableBoard, nextX, nextY)) {
                 solvableBoard[nextX][nextY] = 0;
                 i += 1;
@@ -47,50 +47,32 @@ public class SudokuConfigurator {
 
     // TODO(Khalid): Implement a better algorithm than the brute force version.
     private static int[][] getSolvedGame() {
-        int[][] newBoard = new int[Sudoku.LIMIT][Sudoku.LIMIT];
-        for (int num = 1; num <= Sudoku.LIMIT; num++) {
-            // Keep allocating numbers.
-            allocateValues(num, newBoard);
-        }
-        return newBoard;
+        int[][] board = new int[Sudoku.LIMIT][Sudoku.LIMIT];
+        solve(board);
+        return board;
     }
 
-    /** TODO(Khalid): Refactor to include graph coloring algorithm. */
-    private static void allocateValues(int num, int[][] newBoard) {
-        int numAlloc = 0;
-        List<Coordinates> coordinates = new ArrayList<>();
-        int interrupt = 0;
-        int attempts = 0;
-        while (numAlloc < Sudoku.LIMIT) {
-            // Pragmatic -> fail fast!
-            if (interrupt > 200) {
-                clear(newBoard, coordinates);
-                interrupt = 0;
-                numAlloc = 0;
-                attempts += 1;
-                if (attempts > 500) {
-                    clear(newBoard);
-                    attempts = 0;
-                    num = 1;
-                }
-            }
-            int nextX = RANDOM.nextInt(Sudoku.LIMIT);
-            int nextY = RANDOM.nextInt(Sudoku.LIMIT);
-            if (isSquareEmpty(newBoard, nextX, nextY)) {
-                newBoard[nextX][nextY] = num;
-                if (GameLogic.isMoveValid(newBoard)) {
-                    coordinates.add(new Coordinates(nextX, nextY));
-                    numAlloc += 1;
-                } else {
-                    newBoard[nextX][nextY] = 0;
-                    interrupt += 1;
+    /**
+     * Backtracking algorithm.
+     */
+    private static void solve(int[][] board) {
+        for (int row = 0; row < Sudoku.LIMIT; row++) {
+            for (int col = 0; col < Sudoku.LIMIT; col++) {
+                if (isSquareEmpty(board, row, col)) {
+                    for (int k = Sudoku.START_VALUE; k <= Sudoku.MAX_VALUE; k++) {
+                        board[row][col] = k;
+                        if (isMoveValid(board, row, col)) {
+                            solve(board);
+                        }
+                        board[row][col] = 0;
+                    }
                 }
             }
         }
     }
 
-    private static boolean isSquareEmpty(int[][] board, int x, int y) {
-        return board[x][y] == 0;
+    private static boolean isSquareEmpty(int[][] board, int row, int col) {
+        return board[row][col] == 0;
     }
 
     private static void clear(int[][] board) {
